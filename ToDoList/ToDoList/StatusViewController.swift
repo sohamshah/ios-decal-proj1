2088902356d25c6677409e0834e897935cf65bf7
sohamshah <sohamshah252@gmail.com>
2016-10-20 00:23:16 -0700

    final submission proj1 - late due to internet issues

---
+++ ToDoList/ToDoList/StatusViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StatusViewController.swift
+//  ToDoList
+//
+//  Created by Soham Shah on 10/20/16.
+//  Copyright © 2016 Soham Shah. All rights reserved.
+//
+
+import UIKit
+
+class StatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
